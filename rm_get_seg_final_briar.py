40e368be94aa5dc785aa046032d9dd810e637c3d
Rajat Modi <rajat@local.local>
2023-01-07 22:00:46 +0530

    test

--- rm_get_seg_final_briar.py
+++ rm_get_seg_final_briar.py
@@ -40,6 +40,20 @@ txt_path = 'trainlist_brs1_atleast_32frames_smooth_strt_end_idx_info_158_pids_no
 
 root = Path('/data/project/rm_silhouette')
 n_frames = 65 # to_sample
+score_thresh = 0.5
+alpha = 0.5
+
+cfg = get_cfg()
+add_deeplab_config(cfg)
+add_maskformer2_config(cfg)
+cfg.merge_from_file("configs/coco/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_100ep.yaml")
+cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/maskformer/mask2former/coco/panoptic/maskformer2_swin_large_IN21k_384_bs16_100ep/model_final_f07440.pkl'
+cfg.MODEL.MASK_FORMER.TEST.SEMANTIC_ON = True
+cfg.MODEL.MASK_FORMER.TEST.INSTANCE_ON = True
+cfg.MODEL.MASK_FORMER.TEST.PANOPTIC_ON = True
+predictor = DefaultPredictor(cfg)
+
+
 lines = read_txt(txt_path)
 
 data = []
@@ -81,4 +95,46 @@ for d in data:
         img_path = Path(v_dir)/f_id
         im = cv2.imread(str(img_path))
         print("read im", im.shape)
+        outputs = predictor(im)
+    
+        # v = Visualizer(im[:, :, ::-1], coco_metadata, scale=1.2, instance_mode=ColorMode.IMAGE_BW)
+        
+        # instance_result = v.draw_instance_predictions(outputs["instances"].to("cpu")).get_image()
+        
+        rm_outputs = outputs['instances'].to("cpu")
+        
+        boxes = rm_outputs.pred_boxes
+        scores = rm_outputs.scores
+        classes = rm_outputs.pred_classes
+        masks =rm_outputs.pred_masks
+        masks = rearrange(masks, 'n h w -> h w n')
+
+        # rle = mask_util.encode(np.array(masks, order="F", dtype="uint8"))
+        
+        c = {'boxes':boxes,\
+            'scores':scores,\
+            'classes':classes,\
+            
+        }
+        # print("hello",scores.shape, classes.shape, masks.shape,classes)
+        
+        n_preds = classes.shape[0]
+
+        # h,w,n = masks.shape 
+        # overall_mask = (np.zeros_like((h,w,1))).astype(np.float32)
+        overall_mask = None
+        for i in range(n_preds):
+            c = classes[i] 
+            if c==0:
+                mask = masks[:,:,i].numpy()
+                mask = repeat(mask, 'h w -> h w c', c = 1)
+                if scores[i] > score_thresh:
+                    # print(mask.shape, overall_mask.shape)
+                    if overall_mask is None:
+                        overall_mask = mask
+                    else:
+                        overall_mask+=mask#.astype(np.float)
+        overall_mask = (overall_mask > 0)*1
+        print("overall mask",overall_mask.shape)
+        
         exit(1)
