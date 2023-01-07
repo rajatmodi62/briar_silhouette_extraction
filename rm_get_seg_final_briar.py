8890ed23991471eb41e94dde5911a1e2a22bbd78
Rajat Modi <rajat@local.local>
2023-01-07 21:45:38 +0530

    test

--- rm_get_seg_final_briar.py
+++ rm_get_seg_final_briar.py
@@ -63,6 +63,7 @@ for d in data:
         
         slice = filtered_f_ids[n- n_frames//2: n+ n_frames//2] #sample from the middle 
         print("slice is", slice)
+        print(start, end)
         exit(1)
     else:
         print("not found much freames")
