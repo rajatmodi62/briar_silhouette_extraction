643a35c2c67157c99a59f1f1ef0bd5577afbf682
Rajat Modi <rajat@local.local>
2023-02-06 15:30:54 -0500

    test

--- rm_extract_h5.py
+++ rm_extract_h5.py
@@ -7,6 +7,8 @@ import torch
 import h5py
 
 p =  '/data/project/clip_extraction_brs_bts_v2/afs_brs1_static/brs_bts_version_1/BRS1/G00021/field/500m/wb/G00021_set1_struct_1623185822092_45efd745/frame_000000_000039_clip_0000/clip.h5'
-hf = h5py.File('p', 'r')
-data = hf.get('dataset_name').value
+
+clip_data = h5py.File(p, 'r')
+data = clip_data['data']
+
 print(data.shape)
