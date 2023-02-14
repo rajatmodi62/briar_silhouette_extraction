b48e4bc22be8db44337da44c1e728ee61b6525c3
Rajat Modi <rajat@local.local>
2023-02-14 13:31:07 -0500

    as

--- rm_increase_frame_rate_get_brs1point1.py
+++ rm_increase_frame_rate_get_brs1point1.py
@@ -144,6 +144,8 @@ for done, item in enumerate(filtered_f_ids):
                             else:
                                 overall_mask+=mask#.astype(np.float)
                 overall_mask = (overall_mask > 0)*1
+                print("before", save_root)
+                print("v_folder", v_folder)
                 dest_path = Path(save_root)/'godzilla_silhouette'/v_folder
                 print("dest path",dest_path)
                 dest_path.mkdir(exist_ok=True, parents =True)
