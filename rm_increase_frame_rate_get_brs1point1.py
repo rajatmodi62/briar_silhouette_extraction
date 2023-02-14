866d50256f81926fd1f444f1ea9554ee084be446
Rajat Modi <rajat@local.local>
2023-02-14 14:13:43 -0500

    as

--- rm_increase_frame_rate_get_brs1point1.py
+++ rm_increase_frame_rate_get_brs1point1.py
@@ -148,7 +148,7 @@ for done, item in enumerate(filtered_f_ids):
                 overall_mask = (overall_mask > 0)*1
                 # print("before", save_root)
                 # print("v_folder", v_folder)
-                v_folder = v_folder.split('/')[3:]
+                v_folder = v_folder.split('/')#[3:]
                 v_folder = '/'.join(v_folder)
                 dest_path = Path(save_root)/v_folder
                 # print("dest path",dest_path)
