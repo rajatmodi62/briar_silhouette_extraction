8b3b5ea8a4c02124d763bf912e9da85dd4ed799a
Rajat Modi <rajat@local.local>
2023-02-20 17:52:30 -0500

    ad

--- rm_increase_frame_rate_get_brs1point1_3.py
+++ rm_increase_frame_rate_get_brs1point1_3.py
@@ -200,7 +200,7 @@ for done, item in enumerate(filtered_f_ids):
                 # #     # cv2.imwrite(str(dest_path), np.uint8(overall_mask*255))
                 #     # save_data.append(overall_mask)
                     pred = (pred['sem_seg'].argmax(0) == 0)*1
-                    pred = pred.astype(torch.bool)
+                    pred = pred.type(torch.bool)
                     # pred = pred.to_sparse_csr()
                     save_data.append(pred)
                     # save_data.append(pred['sem_seg'].argmax(0))#.to('cpu').numpy())
